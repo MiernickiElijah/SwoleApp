6e99460c6f893ef5b417e86fac8035ea36ff23a9
Elijah Miernicki <miernickielijah@gmail.com>
2021-08-15 14:20:28 -0500

    created server

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-const router = require('express').Router();
+const express = require("express");
+const mongoose = require("mongoose");
 
-const apiRoutes = require('./api');
-const htmlRoutes = require('./htmlRoutes');
+const PORT = process.env.PORT || 3001
 
-router.use('/', htmlRoutes);
-router.use('/api', apiRoutes);
-router.use((req, res) => {
-    res.status(404).end();
-})
+const app = express();
 
-module.exports = router;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+//app.use(require("./routes/api.js"));
+//app.use(require("./routes/htmlroutes.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
